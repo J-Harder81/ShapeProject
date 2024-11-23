eca6c1220fc0ba1a01ed55cd6ae22278fece1ad3
Justi <Justi@HYPER_Z-FURY>
2024-11-23 10:51:59 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * CSC180 Week12 Assignment #1
+ * key concepts: abstract class/method; inheritance; constructor; array of objects
+ * Tester: Program.cs
+ */
+using System;
+using System.Drawing;
+public class Program
+{
+    public static void Main()
+    {   // Creates an array called shapes and stores the new shapes and their sizes to calculate the area
+        Shape[] shapes = { new Square(10), new Circle(10), new Rectangle(5, 10), new Triangle(15, 10) };
+        foreach (var shape in shapes)   // Iterates over each shape in the array
+        {
+            Console.WriteLine($"Area of {shape}: {shape.Area()}");  // Envokes the Area method returning the overridden area calculation for each shape class
+            Console.WriteLine($"Area of {shape}: {Shape.GetArea(shape)}, again"); /* Envokes the static GetArea method taking the shape as a parameter 
+                                                                                   * returning the same area calculation as the Area method */
+        }
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * CSC180 Week12 Assignment #1
+ * key concepts: abstract class/method; inheritance; constructor; array of objects
+ * Shape class: Shape.cs
+ */
+
+using System;
+
+public abstract class Shape
+{
+    public abstract double Area();
+    public static double GetArea(Shape shape)
+    {
+        return shape.Area();
+    }
+}
+
+public class Square : Shape
+{
+    private double size;
+    public Square(double length)
+    {
+        this.size = length;
+    }
+    public override double Area()
+    {
+        return Math.Pow(size, 2);
+    }
+}
+
+public class Circle : Shape
+{
+    private double radius;
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+    public override double Area()
+    {
+        return Math.Round(Math.PI * Math.Pow(radius, 2), 2);
+    }
+}
+// Add Rectangle and Triangle classes that inherit Shape
+public class Rectangle : Shape
+{
+    private double width;
+    private double length;
+    public Rectangle(double width, double length)
+    {
+        this.width = width;
+        this.length = length;
+    }
+    public override double Area()
+    {
+        return width * length;
+    }
+}
+public class Triangle : Shape
+{
+    private double baseLength;
+    private double height;
+    public Triangle(double baseLength, double height)
+    {
+        this.baseLength = baseLength;
+        this.height = height;
+    }
+    public override double Area()
+    {
+        return 0.5 * baseLength * height;
+    }
+}
